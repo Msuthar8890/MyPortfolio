c0af953464e81341e29d8558936b99bece995be0
MANOJ SUTHAR <115114118+Msuthar8890@users.noreply.github.com>
2024-06-21 18:08:13 +0530

    sum

--- js/main.js
+++ js/main.js
@@ -9,37 +9,10 @@ var typed = new Typed(".text", {
 
 
 
+// scripts.js
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.getElementById('nav-links');
 
-
-// const slider = document.querySelector('.portfolio-content');
-// let isSliding = true;
-
-// function autoSlide() {
-//     if (!isSliding) return;
-
-//     const firstCard = slider.firstElementChild;
-//     const cardWidth = firstCard.offsetWidth;
-
-//     slider.style.transition = 'transform 0.5s ease-in-out';
-//     slider.style.transform = `translateX(-${cardWidth}px)`;
-
-//     slider.addEventListener('transitionend', () => {
-//         slider.style.transition = 'none';
-//         slider.appendChild(firstCard);
-//         slider.style.transform = 'translateX(0)';
-
-//         // Re-enable the sliding
-//         if (isSliding) {
-//             setTimeout(autoSlide, 2000); // Adjust the interval as needed
-//         }
-//     }, { once: true });
-// }
-
-// slider.addEventListener('mouseover', () => isSliding = false);
-// slider.addEventListener('mouseout', () => {
-//     isSliding = true;
-//     setTimeout(autoSlide, 2000); // Restart the sliding
-// });
-
-// // Initial start of auto sliding
-// setTimeout(autoSlide, 2000); // Start the sliding after a delay
+mobileMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
